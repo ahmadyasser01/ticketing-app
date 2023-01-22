91a110fabdfbc33876c3a3d28d90f9b46a97e8f0
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-01-23 00:03:04 +0200

    fix:update ts config file

--- common/src/types/Express/custom.d.ts
+++ common/src/types/Express/custom.d.ts
@@ -1,5 +1,10 @@
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
 declare namespace Express {
     export interface Request {
-      currentUser?: string
+      currentUser?: UserPayload
     }
  }
