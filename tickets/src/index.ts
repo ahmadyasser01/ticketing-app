065eb4afc4e0b81b7c4ea0f5a4044679c87a1d9c
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-02-19 23:30:42 +0200

    listen to events

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
+import { natsWrapper } from "./nats-wrapper";
 
 //TODO: ADD LISTENER TO INDEX.TS
 
@@ -11,7 +14,6 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must be defined");
   }
-  //TODO: CHECK FOR  NATS ENVIROMENT VARAIABLES
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error("NATS CLIENT ID must be defined");
   }
@@ -22,7 +24,22 @@ const start = async () => {
     throw new Error("NATS CLUSTER ID must be defined");
   }
   try {
-    //TODO:  LISTEN TO EVENTS
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
+
     await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.log(error);
