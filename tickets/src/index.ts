23853874478e4fe68d3bf41bea627fb27905e5a5
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-02-19 23:27:22 +0200

    feat:check for nats process env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,6 +12,15 @@ const start = async () => {
     throw new Error("MONGO_URI must be defined");
   }
   //TODO: CHECK FOR  NATS ENVIROMENT VARAIABLES
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS CLIENT ID must be defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS URL ID must be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS CLUSTER ID must be defined");
+  }
   try {
     //TODO:  LISTEN TO EVENTS
     await mongoose.connect(process.env.MONGO_URI);
