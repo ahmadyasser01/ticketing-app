e1cbaf540006a1727e012aa70961d76d6fd8d078
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-02-23 10:38:31 +0200

    feat:payment created event publisher

---
+++ payments/src/events/publishers/payment-created-publisher.ts
@@ -0,0 +1,9 @@
+import {
+  Subjects,
+  Publisher,
+  PaymentCreatedEvent,
+} from "@ahmadyasser01/common";
+
+export class PaymentCreatedPublisher extends Publisher<PaymentCreatedEvent> {
+  subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+}
