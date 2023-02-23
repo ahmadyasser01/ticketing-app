57498da029bd5db929940c08aaa2a374caa9f4f0
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-02-23 11:34:17 +0200

    use create charge router

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -7,6 +7,7 @@ import {
   NotFoundError,
   currentUser,
 } from "@ahmadyasser01/common";
+import { createChargeRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -19,7 +20,7 @@ app.use(
 );
 app.use(currentUser);
 
-//TODO: ADD ROUTES
+app.use(createChargeRouter);
 app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
