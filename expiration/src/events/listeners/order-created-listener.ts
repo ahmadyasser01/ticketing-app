66371bf10ca83a331ebc50306c5df66da34c0a72
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-02-26 12:41:11 +0200

    order-created-listener

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,18 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@ahmadyasser01/common";
+import { OrderStatus } from "@ahmadyasser01/common/build/events/Interfaces/order-status";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListerner extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName: string = queueGroupName;
+  async onMessage(
+    data: OrderCreatedEvent["data"],
+    msg: Message
+  ): Promise<void> {
+    //TODO: CREATE DELAY
+    //TODO: ADD TO QUEUE THE DELAY
+
+    msg.ack();
+  }
+}
