0341fd01e544b19c2b94eb388a96bd2c5c33200d
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-02-26 13:49:37 +0200

    feat: add timer to expiration queue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,6 +1,7 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@ahmadyasser01/common";
 import { OrderStatus } from "@ahmadyasser01/common/build/events/Interfaces/order-status";
 import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
 import { queueGroupName } from "./queue-group-name";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
@@ -10,8 +11,9 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     data: OrderCreatedEvent["data"],
     msg: Message
   ): Promise<void> {
-    //TODO: CREATE DELAY
-    //TODO: ADD TO QUEUE THE DELAY
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log("Waiting ", delay, " milliseconds to process the job");
+    await expirationQueue.add({ orderId: data.id }, { delay });
 
     msg.ack();
   }
